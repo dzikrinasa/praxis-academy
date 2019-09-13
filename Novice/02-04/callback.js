cfde9f4fdf10dd33ce68c8e9411ff5cd6e0675cd
dzikrinasa <dzikrinandiasa@gmail.com>
2019-09-13 11:14:09 +0700

    Hari ke 3 minggu 2

---
+++ Novice/02-04/callback.js
@@ -0,0 +1,16 @@
+function main(param1,param2,callBack){
+  console.log(param1, param2)
+  callBack()
+}
+
+function myCallback(){
+  console.log ('hello callback')
+}
+
+main(1,2,myCallback)
+
+/* ===================
+Output :
+ 1 2
+ hello callback
+ */
