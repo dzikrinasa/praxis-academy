dc20b384ad96df4e555b3039c6d92d091a177621
dzikrinasa <dzikrinandiasa@gmail.com>
2019-09-29 14:39:27 +0700

    minggu 4 hari 5

---
+++ Novice/04-05/axiosAPI.js
@@ -0,0 +1,19 @@
+// Send a POST request
+axios({
+    method: 'post',
+    url: '/user/12345',
+    data: {
+      firstName: 'Fred',
+      lastName: 'Flintstone'
+    }
+  });
+  
+  // GET request for remote image
+  axios({
+    method: 'get',
+    url: 'http://bit.ly/2mTM3nY',
+    responseType: 'stream'
+  })
+    .then(function (response) {
+      response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+    });
